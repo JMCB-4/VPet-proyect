2e0989f772be468e8c7fcecdbff934d367987a7b
jmcb-4 <juanmanuel.cruzberjano@gmail.com>
2024-12-06 17:53:19 +0100

    first interface implemented

--- src/main.js
+++ src/main.js
@@ -1,11 +1,37 @@
 import kaplay from "kaplay";
-// import "kaplay/global"; // uncomment if you want to use without the k. prefix
+import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
-const k = kaplay();
+kaplay({
+    background: "#cdecfa",
+    width: 640,
+    height: 640,
+});
 
-k.loadRoot("./"); // A good idea for Itch.io publishing later
-k.loadSprite("bean", "sprites/bean.png");
+loadRoot("./"); // A good idea for Itch.io publishing later
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
+// loadSprite for the sprite of the vpet
+loadSprite("vpet", "sprites/vpet.png");
+loadSprite("bttn1", "sprites/button1.png");
+loadSprite("bttn2", "sprites/button2.png");
 
-k.onClick(() => k.addKaboom(k.mousePos()));
+// Creaction of the const for the vpet
+const vpet = add([
+    sprite("vpet"),
+    pos(width()/2 - 40, height()/2 - 40),
+    anchor("center"),
+    scale(3), 
+]);
+// Creation of the button for water
+const button1 = add([
+    sprite("bttn1"),
+    pos(60 ,height() - 200),
+    scale(2),
+    area(),
+]);
+// Creation of the button to check on thirst
+const button2 = add([
+    sprite("bttn2"),
+    pos(420,height() - 200),
+    scale(2),
+    area(),
+]);
