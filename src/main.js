d9b729f4d1f16b90f4616929820377dd5a3a2cc6
JMCB-4 <juanmanuelcruzberjano@gmail.com>
2024-12-11 16:26:58 +0100

    update code comments

--- src/main.js
+++ src/main.js
@@ -2,66 +2,66 @@ import kaplay from "kaplay";
 import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
 kaplay({
-    background: "#cdecfa",
-    width: 640,
-    height: 640,
+    background: "#cdecfa", // changes the background to a solic color
+    width: 640, // initialize the width
+    height: 640, // initialize the height
 });
 
 loadRoot("./"); // a good idea for Itch.io publishing later
 
-// load the sprite for the VPET and buttons
-loadSprite("vpet", "sprites/vpet.png");
+// loads the necessary sprites
+loadSprite("vpet", "sprites/vpet.png"); 
 loadSprite("bttn1", "sprites/button1.png");
 loadSprite("bttn2", "sprites/button2.png");
 
 // create the const for the VPET
 const vpet = add([
-    sprite("vpet"),
-    anchor("center"),
-    scale(3),
-    animate(), 
+    sprite("vpet"), // assigns a sprite
+    anchor("center"), // set the anchor point to the center of the sprite
+    scale(3), // scale the sprite to make it bigger
+    animate(),  // allows animation on the object
    
 ]);
 
 // create a button to give water
 const button1 = add([
-    sprite("bttn1"),
-    pos(150 ,height() - 150),
-    scale(2),
-    area(),
-    anchor("center"),
+    sprite("bttn1"), // assigns a sprite
+    pos(150 ,height() - 150), // set the possition
+    scale(2), // scale the sprite to make it bigger
+    area(), // create an area to allow things like hover or click
+    anchor("center"), // set the anchor point to the center of the sprite
     
 ]);
 
 // create a button to check thirst
 const button2 = add([
-    sprite("bttn2"),
-    pos(520,height() - 150),
-    scale(2),
-    area(),
-    anchor("center"),
+    sprite("bttn2"), // assigns a sprite
+    pos(520,height() - 150), // set the possition
+    scale(2), // scale the sprite to make it bigger
+    area(), // create an area to allow things like hover or click
+    anchor("center"), // set the anchor point to the center of the sprite
     
 ]);
 
 // animation for the VPET
-vpet.animate("pos", [vec2(300, 280), vec2(300, 260)], {
-    duration: 2,
-    direction: "ping-pong",
+vpet.animate("pos", [vec2(300, 280), vec2(300, 260)], { // animate the vpet giving it movement
+    duration: 2, // duration of the animation
+    direction: "ping-pong", // type of animation
 });
 
 // add a selection effect to the button on hover
-button1.onHoverUpdate(() => {
-    button1.scale = vec2(2.2);
+button1.onHoverUpdate(() => { 
+    button1.scale = vec2(2.2); // bigger button on hover
 });
 // end the effect when the hover ends
 button1.onHoverEnd(() => {
-    button1.scale = vec2(2)
+    button1.scale = vec2(2) // reset set scale when hover ends
 });
 // add a selection effect to the button on hover
 button2.onHoverUpdate(() => {
-    button2.scale = vec2(2.2);
+    button2.scale = vec2(2.2); // bigger button on hover
 });
 // end the effect when the hover ends
 button2.onHoverEnd(() => {
-    button2.scale = vec2(2)
+    button2.scale = vec2(2) // reset set scale when hover ends
 });
